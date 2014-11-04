b59f5de1d6e456c3a5824982ac6f23b767afebac
ozg <ouzhigangGBA@gmail.com>
2014-11-04 23:17:39 +0800

    update to cocos2d-x-3.3rc0

--- proj.android/gen/R.java.d
+++
@@ -1,6 +0,0 @@
-D:\root\Temp\OzgGameEatFish\proj.android\gen\com\ozggame\eatfish\R.java \
- : D:\root\Temp\OzgGameEatFish\proj.android\res\drawable-hdpi\icon.png \
-D:\root\Temp\OzgGameEatFish\proj.android\res\drawable-ldpi\icon.png \
-D:\root\Temp\OzgGameEatFish\proj.android\res\drawable-mdpi\icon.png \
-D:\root\Temp\OzgGameEatFish\proj.android\res\values\strings.xml \
-D:\root\Temp\OzgGameEatFish\proj.android\bin\AndroidManifest.xml \
