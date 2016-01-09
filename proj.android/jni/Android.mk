b5b163c0f27fa56a9d6f8070724caa7837dd877f
ouzhigang <ouzhiganggba@gmail.com>
2016-01-09 20:52:42 +0800

    优化代码

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -26,10 +26,6 @@ FILE_LIST += $(filter %.cpp, $(ALLFILES))
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../Classes/ozgcc
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../Classes/eatfish
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../Classes/eatfish/element
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../Classes/eatfish/scene
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../cocos2d
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../cocos2d/extensions
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../cocos2d/cocos/ui
